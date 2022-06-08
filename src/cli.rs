4976ebe2e6ee8c07805ddf4ce7c1aaa5aa4016dc
esteinig <eike.steinig@unimelb.edu.au>
2022-06-08 18:01:30 +1000

    add vircov sucommand

--- src/cli.rs
+++ src/cli.rs
@@ -2,173 +2,181 @@ use std::ffi::{OsStr, OsString};
 use std::path::PathBuf;
 use structopt::StructOpt;
 
-// Vircov command-line client
+/// Metacov Command Line client
 #[derive(Debug, StructOpt)]
-#[structopt(name = "metacov")]
+#[structopt(name = "lab-tools")]
 pub struct Cli {
-    /// Alignment file (SAM/BAM/CRAM/PAF)
-    #[structopt(
-        short, long, parse(try_from_os_str = check_file_exists), required = true
-    )]
-    pub alignment: PathBuf,
-    /// bam: SAM/BAM/CRAM alignment; paf: PAF alignment
-    ///
-    /// Default is to attempt to infer the input alignment format automatically from the filename
-    /// extension (bam|sam|cram|paf). This option is used to override that.
-    #[structopt(
-        short = "A",
-        long,
-        value_name = "bam|paf",
-        possible_values = &["bam", "paf"],
-        case_insensitive=true,
-        hide_possible_values=true
-    )]
-    pub alignment_format: Option<String>,
-    /// Reference sequences in FASTA format [default: None]
-    ///
-    /// If the input format is PAF, computation of the total coverage
-    /// against each target sequence requires sequence lengths. Must
-    /// be the same reference file as used for the alignment.
-    #[structopt(
-        short = "f",
-        long = "fasta", 
-        parse(try_from_os_str = check_file_exists)
-    )]
-    pub fasta: Option<PathBuf>,
-    /// Minimum length of the aligned query sequence
-    ///
-    /// Filters (&) alignments by minimum length of the aligned query sequence,
-    /// which corresponds to the difference between query alignment end and
-    /// start positions.
-    #[structopt(short = "L", long, default_value = "0")]
-    pub min_len: u64,
-    /// Minimum coverage of the aligned query sequence
-    ///
-    /// Filters (&) alignments by minimum proportion of the query sequence involved
-    /// in the alignment which corresponds to division of the length of the
-    /// aligned query sequence by the length of the query sequence.
-    #[structopt(short = "C", long, default_value = "0")]
-    pub min_cov: f64,
-    /// Minimum mapping quality of the alignment
-    ///
-    /// Filters (&) alignments by a minimum mapping quality.
-    #[structopt(short = "M", long, default_value = "0")]
-    pub min_mapq: u8,
-    /// Verbose output statistics  
-    ///
-    /// Single flag (-v) adds whitespace separated tags in the last column,
-    /// corresponding to the number of inferred alignment coverage regions.
-    /// Tag fields are separated by semicolons.
-    ///
-    /// When no grouping argument is given then each tag consists of
-    ///     : start of coverage region
-    ///     : end of coverage region
-    ///     : number of alignments in the region
-    ///
-    /// When the output is grouped, then each tag consists of data from each
-    /// output that has been grouped
-    ///     : name of reference sequence
-    ///     : number of regions
-    ///     : number of reads in region
-    ///     : number of alignments in region
-    ///     : number of base pairs in region
-    ///     : length of reference sequence
-    ///     : coverage on reference sequence
-    #[structopt(short, long, parse(from_occurrences = parse_verbosity))]
-    pub verbose: u64,
-    /// Group outputs by a field in the reference sequence description
-    ///
-    /// Grouping can help to summarize regions and other output statistics
-    /// aross all members of the group. Value specified as group should be
-    /// in the target sequence headers separated by a delimiter, for example
-    /// `taxid=1101 | species=MeanVirus` where one can group by either the
-    /// `taxid=` or `species=` fields (including comma). Alignment target
-    /// sequence are split by the delimiter `|` and the field value is
-    /// extracted for each alignment. Groups are summarized as follows:
-    /// distinct regions are summed, alignments are summed, unique reads are
-    /// recomputed across all members of the group, covered base pairs in the
-    /// alignments and reference sequence lengths are set to 0, and coverage
-    /// across all target sequences in the group is averaged.
-    #[structopt(short, long)]
-    pub group_by: Option<String>,
-    /// Group field separator in the reference sequence description
-    ///
-    /// Often there are multiple fields in the header of the reference
-    /// sequences (e.g. taxid=1101 | taxname=argh). A delimitor needs to
-    /// be specified to signal the end of a specific grouping field
-    /// (e.g. --group-sep "|" with --group-by "taxid=").
-    #[structopt(long, default_value = ";")]
-    pub group_sep: String,
-    /// A file with a string per line to exclude alignments
-    /// if the string occurs in the target sequence description
-    ///
-    /// This option can be used as a blacklist to filter out
-    /// alignments of unwanted viruses, e.g. using taxonomy
-    /// identifiers or species names in target sequence headers.
-    #[structopt(short, long, parse(try_from_os_str = check_file_exists))]
-    pub exclude: Option<PathBuf>,
-    /// Prints pretty output table  
-    ///
-    /// Output the coverage statistics as a pretty table; may still get
-    /// mangled on short terminals, as terminal width cannot currently
-    /// be estimated in underlying library.
-    #[structopt(short = "t", long = "table")]
-    pub table: bool,
-    /// Prints coverage plots
-    ///
-    /// Output coverage plots below the coverage statistics.
-    #[structopt(short = "k", long = "cov-plot")]
-    pub covplot: bool,
-    /// Width of coverage plots
-    ///
-    /// Adjusts the (approximate) width of the coverage plots by
-    /// computing the bases covered by each coverage segment.
-    #[structopt(short = "w", long = "width", default_value = "100")]
-    pub width: u64,
-    /// Minimum reference sequence length
-    ///
-    /// Filters results by minimum reference sequence length
-    /// which can help remove alignments against small genes
-    /// or genome fragments.
-    #[structopt(short = "l", long = "length", default_value = "0")]
-    pub seq_len: u64,
-    /// Minimum number of coverage regions
-    ///
-    /// Filters results by a minimum number of coverage regions, the
-    /// primary output to determine a positive hit.
-    #[structopt(short = "r", long = "regions", default_value = "0")]
-    pub regions: u64,
-    /// Minimum read threshold (unique reads in alignment)
-    ///
-    /// Filters results by a minimum reads in alignment; if results
-    /// are grouped this is done before the grouping stage to weed
-    /// out spurious alignments
-    #[structopt(short = "u", long = "reads", default_value = "0")]
-    pub reads: u64,
-    /// Minimum coverage threshold (fraction)
-    ///
-    /// Filters results by a minimum coverage across the reference sequence
-    #[structopt(short = "c", long = "coverage", default_value = "0")]
-    pub coverage: f64,
-    /// Minimum number of grouped coverage regions
-    ///
-    /// Filters results by a minimum number of grouped coverage regions, can be
-    /// used in addition to the pre-grouping coverage region filter
-    #[structopt(long, default_value = "0")]
-    pub group_regions: u64,
-    /// Minimum grouped coverage threshold (fraction)
-    ///
-    /// Filters results by a minimum average coverage
-    /// across reference sequences if results are grouped
-    #[structopt(long, default_value = "0")]
-    pub group_coverage: f64,
-    /// Output read identifiers of aligned reads to file
-    ///
-    /// Creates a file that contains the (unique) identifiers
-    /// of all reads that passed the alignment and coverage
-    /// filters (i.e. those involved in the table outputs)
-    #[structopt(short = "O", long = "read-ids")]
-    pub read_ids: Option<PathBuf>,
+    #[structopt(subcommand)]
+    pub commands: Commands,
+}
+
+#[derive(Debug, StructOpt)]
+pub enum Commands {
+    Vircov {
+        /// Alignment file (SAM/BAM/CRAM/PAF)
+        #[structopt(
+            short, long, parse(try_from_os_str = check_file_exists), required = true
+        )]
+        alignment: PathBuf,
+        /// bam: SAM/BAM/CRAM alignment; paf: PAF alignment
+        ///
+        /// Default is to attempt to infer the input alignment format automatically from the filename
+        /// extension (bam|sam|cram|paf). This option is used to override that.
+        #[structopt(
+            short = "A",
+            long,
+            value_name = "bam|paf",
+            possible_values = &["bam", "paf"],
+            case_insensitive=true,
+            hide_possible_values=true
+        )]
+        alignment_format: Option<String>,
+        /// Reference sequences in FASTA format [default: None]
+        ///
+        /// If the input format is PAF, computation of the total coverage
+        /// against each target sequence requires sequence lengths. Must
+        /// be the same reference file as used for the alignment.
+        #[structopt(
+            short = "f",
+            long = "fasta", 
+            parse(try_from_os_str = check_file_exists)
+        )]
+        fasta: Option<PathBuf>,
+        /// Minimum length of the aligned query sequence
+        ///
+        /// Filters (&) alignments by minimum length of the aligned query sequence,
+        /// which corresponds to the difference between query alignment end and
+        /// start positions.
+        #[structopt(short = "L", long, default_value = "0")]
+        min_len: u64,
+        /// Minimum coverage of the aligned query sequence
+        ///
+        /// Filters (&) alignments by minimum proportion of the query sequence involved
+        /// in the alignment which corresponds to division of the length of the
+        /// aligned query sequence by the length of the query sequence.
+        #[structopt(short = "C", long, default_value = "0")]
+        min_cov: f64,
+        /// Minimum mapping quality of the alignment
+        ///
+        /// Filters (&) alignments by a minimum mapping quality.
+        #[structopt(short = "M", long, default_value = "0")]
+        min_mapq: u8,
+        /// Verbose output statistics  
+        ///
+        /// Single flag (-v) adds whitespace separated tags in the last column,
+        /// corresponding to the number of inferred alignment coverage regions.
+        /// Tag fields are separated by semicolons.
+        ///
+        /// When no grouping argument is given then each tag consists of
+        ///     : start of coverage region
+        ///     : end of coverage region
+        ///     : number of alignments in the region
+        ///
+        /// When the output is grouped, then each tag consists of data from each
+        /// output that has been grouped
+        ///     : name of reference sequence
+        ///     : number of regions
+        ///     : number of reads in region
+        ///     : number of alignments in region
+        ///     : number of base pairs in region
+        ///     : length of reference sequence
+        ///     : coverage on reference sequence
+        #[structopt(short, long, parse(from_occurrences = parse_verbosity))]
+        verbose: u64,
+        /// Group outputs by a field in the reference sequence description
+        ///
+        /// Grouping can help to summarize regions and other output statistics
+        /// aross all members of the group. Value specified as group should be
+        /// in the target sequence headers separated by a delimiter, for example
+        /// `taxid=1101 | species=MeanVirus` where one can group by either the
+        /// `taxid=` or `species=` fields (including comma). Alignment target
+        /// sequence are split by the delimiter `|` and the field value is
+        /// extracted for each alignment. Groups are summarized as follows:
+        /// distinct regions are summed, alignments are summed, unique reads are
+        /// recomputed across all members of the group, covered base pairs in the
+        /// alignments and reference sequence lengths are set to 0, and coverage
+        /// across all target sequences in the group is averaged.
+        #[structopt(short, long)]
+        group_by: Option<String>,
+        /// Group field separator in the reference sequence description
+        ///
+        /// Often there are multiple fields in the header of the reference
+        /// sequences (e.g. taxid=1101 | taxname=argh). A delimitor needs to
+        /// be specified to signal the end of a specific grouping field
+        /// (e.g. --group-sep "|" with --group-by "taxid=").
+        #[structopt(long, default_value = ";")]
+        group_sep: String,
+        /// A file with a string per line to exclude alignments
+        /// if the string occurs in the target sequence description
+        ///
+        /// This option can be used as a blacklist to filter out
+        /// alignments of unwanted viruses, e.g. using taxonomy
+        /// identifiers or species names in target sequence headers.
+        #[structopt(short, long, parse(try_from_os_str = check_file_exists))]
+        exclude: Option<PathBuf>,
+        /// Prints pretty output table  
+        ///
+        /// Output the coverage statistics as a pretty table; may still get
+        /// mangled on short terminals, as terminal width cannot currently
+        /// be estimated in underlying library.
+        #[structopt(short = "t", long = "table")]
+        table: bool,
+        /// Prints coverage plots
+        ///
+        /// Output coverage plots below the coverage statistics.
+        #[structopt(short = "k", long = "cov-plot")]
+        covplot: bool,
+        /// Width of coverage plots
+        ///
+        /// Adjusts the (approximate) width of the coverage plots by
+        /// computing the bases covered by each coverage segment.
+        #[structopt(short = "w", long = "width", default_value = "100")]
+        width: u64,
+        /// Minimum reference sequence length
+        ///
+        /// Filters results by minimum reference sequence length
+        /// which can help remove alignments against small genes
+        /// or genome fragments.
+        #[structopt(short = "l", long = "length", default_value = "0")]
+        seq_len: u64,
+        /// Minimum number of coverage regions
+        ///
+        /// Filters results by a minimum number of coverage regions, the
+        /// primary output to determine a positive hit.
+        #[structopt(short = "r", long = "regions", default_value = "0")]
+        regions: u64,
+        /// Minimum read threshold (unique reads in alignment)
+        ///
+        /// Filters results by a minimum reads in alignment; if results
+        /// are grouped this is done before the grouping stage to weed
+        /// out spurious alignments
+        #[structopt(short = "u", long = "reads", default_value = "0")]
+        reads: u64,
+        /// Minimum coverage threshold (fraction)
+        ///
+        /// Filters results by a minimum coverage across the reference sequence
+        #[structopt(short = "c", long = "coverage", default_value = "0")]
+        coverage: f64,
+        /// Minimum number of grouped coverage regions
+        ///
+        /// Filters results by a minimum number of grouped coverage regions, can be
+        /// used in addition to the pre-grouping coverage region filter
+        #[structopt(long, default_value = "0")]
+        group_regions: u64,
+        /// Minimum grouped coverage threshold (fraction)
+        ///
+        /// Filters results by a minimum average coverage
+        /// across reference sequences if results are grouped
+        #[structopt(long, default_value = "0")]
+        group_coverage: f64,
+        /// Output read identifiers of aligned reads to file
+        ///
+        /// Creates a file that contains the (unique) identifiers
+        /// of all reads that passed the alignment and coverage
+        /// filters (i.e. those involved in the table outputs)
+        #[structopt(short = "O", long = "read-ids")]
+        read_ids: Option<PathBuf>,
+    },
 }
 
 fn check_file_exists(file: &OsStr) -> Result<PathBuf, OsString> {
@@ -184,7 +192,7 @@ fn check_file_exists(file: &OsStr) -> Result<PathBuf, OsString> {
 ///
 /// Up to one verbosity flags are allowed (-v), if more
 /// are specified (-vv) the highest value is returned
-pub fn parse_verbosity(v: u64) -> u64 {
+fn parse_verbosity(v: u64) -> u64 {
     match v {
         0 | 1 | 2 => v,
         _ => 2,
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use structopt::StructOpt;
 
 use crate::align::{ReadAlignment, ReadAlignmentError};
 use crate::cli::Cli;
+use crate::cli::Commands::Vircov;
 
 mod align;
 mod cli;
@@ -15,56 +16,69 @@ mod covplot;
 #[cfg(not(tarpaulin_include))]
 fn main() -> Result<(), ReadAlignmentError> {
     let args = Cli::from_args();
-    let mut alignment = ReadAlignment::new(args.fasta, args.exclude)?;
+    match args.commands {
+        Vircov {
+            fasta,
+            alignment,
+            exclude,
+            min_len,
+            min_cov,
+            min_mapq,
+            alignment_format,
+            regions,
+            seq_len,
+            coverage,
+            group_by,
+            reads,
+            verbose,
+            table,
+            read_ids,
+            covplot,
+            group_regions,
+            group_coverage,
+            group_sep,
+            width,
+        } => {
+            let mut _alignment = ReadAlignment::new(fasta, exclude)?;
 
-    let alignment = alignment.from(
-        args.alignment,
-        args.min_len,
-        args.min_cov,
-        args.min_mapq,
-        args.alignment_format,
-    )?;
+            let alignment =
+                _alignment.from(alignment, min_len, min_cov, min_mapq, alignment_format)?;
 
-    let data = alignment.coverage_statistics(
-        args.regions,
-        args.seq_len,
-        args.coverage,
-        args.reads,
-        &args.group_by,
-        args.verbose,
-    )?;
+            let data = alignment
+                .coverage_statistics(regions, seq_len, coverage, reads, &group_by, verbose)?;
 
-    match args.group_by {
-        None => {
-            alignment.to_output(&data, args.table, args.read_ids)?;
-        }
-        Some(group_field) => {
-            match alignment.target_sequences {
-                None => return Err(ReadAlignmentError::GroupSequenceError()),
-                Some(_) => {
-                    match args.covplot {
-                        true => return Err(ReadAlignmentError::GroupCovPlotError()),
-                        false => {
-                            // If reference sequences have been provided, continue with grouping outputs
-                            let grouped_data = alignment.group_output(
-                                &data,
-                                args.group_regions,
-                                args.group_coverage,
-                                group_field,
-                                args.group_sep,
-                            )?;
-                            alignment.to_output(&grouped_data, args.table, args.read_ids)?;
-                        }
-                    };
+            match group_by {
+                None => {
+                    alignment.to_output(&data, table, read_ids)?;
                 }
+                Some(group_field) => {
+                    match alignment.target_sequences {
+                        None => return Err(ReadAlignmentError::GroupSequenceError()),
+                        Some(_) => {
+                            match covplot {
+                                true => return Err(ReadAlignmentError::GroupCovPlotError()),
+                                false => {
+                                    // If reference sequences have been provided, continue with grouping outputs
+                                    let grouped_data = alignment.group_output(
+                                        &data,
+                                        group_regions,
+                                        group_coverage,
+                                        group_field,
+                                        group_sep,
+                                    )?;
+                                    alignment.to_output(&grouped_data, table, read_ids)?;
+                                }
+                            };
+                        }
+                    }
+                }
+            };
+
+            match covplot {
+                true => alignment.coverage_plots(&data, width)?,
+                false => {}
             }
         }
-    };
-
-    match args.covplot {
-        true => alignment.coverage_plots(&data, args.width)?,
-        false => {}
     }
-
     Ok(())
 }
