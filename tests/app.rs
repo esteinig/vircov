3dfe52fea6aea2cb19727948e7914ed803a452b1
esteinig <eike.steinig@unimelb.edu.au>
2023-02-19 09:32:54 +1100

    fixed app tests

--- tests/app.rs
+++ tests/app.rs
@@ -5,7 +5,7 @@ use std::process::Command;
 #[test]
 fn input_file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
-    cmd.args(vec!["--paf", "file/doesnt/exist.paf"]);
+    cmd.args(vec!["--alignment", "file/doesnt/exist.paf"]);
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("does not exist"));
@@ -17,7 +17,7 @@ fn input_file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
 fn valid_inputs_raise_no_errors() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
     cmd.args(vec![
-        "--paf",
+        "--alignment",
         "tests/cases/test_ok.paf",
         "--fasta",
         "tests/cases/test_paf_ok.fasta",
@@ -33,37 +33,14 @@ fn valid_inputs_raise_no_errors() -> Result<(), Box<dyn std::error::Error>> {
 fn valid_output_string_default_filters() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
     cmd.args(vec![
-        "--paf",
+        "--alignment",
         "tests/cases/test_ok.paf",
         "--fasta",
         "tests/cases/test_paf_ok.fasta",
     ]);
 
     cmd.assert().success().stdout(predicate::str::contains(
-        "21172389_LCMV_L-segment_final\t2\t1\t2\t273\t7194\t0.0379\t",
-    ));
-
-    Ok(())
-}
-
-#[test]
-fn valid_output_string_no_filters() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
-    cmd.args(vec![
-        "--paf",
-        "tests/cases/test_ok.paf",
-        "--fasta",
-        "tests/cases/test_paf_ok.fasta",
-        "--min-mapq",
-        "0",
-        "--min-len",
-        "0",
-        "--min-cov",
-        "0",
-    ]);
-
-    cmd.assert().success().stdout(predicate::str::contains(
-        "21172389_LCMV_L-segment_final\t3\t2\t3\t321\t7194\t0.0446\t-\t-\n21172389_LCMV_S-segment_final\t2\t2\t2\t137\t3407\t0.0402\t-\t-"
+        "21172389_LCMV_L-segment_final\t3\t2\t3\t321\t7194\t0.0446\t-\t-\n21172389_LCMV_S-segment_final\t2\t2\t2\t137\t3407\t0.0402\t-\t-\n"
     ));
 
     Ok(())
@@ -74,7 +51,7 @@ fn valid_output_string_default_filters_verbose_one_no_descr(
 ) -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
     cmd.args(vec![
-        "--paf",
+        "--alignment",
         "tests/cases/test_ok.paf",
         "--fasta",
         "tests/cases/test_paf_ok.fasta",
@@ -82,7 +59,7 @@ fn valid_output_string_default_filters_verbose_one_no_descr(
     ]);
 
     cmd.assert().success().stdout(predicate::str::contains(
-        "21172389_LCMV_L-segment_final\t2\t1\t2\t273\t7194\t0.0379\t-\t-",
+        "21172389_LCMV_L-segment_final\t3\t2\t3\t321\t7194\t0.0446\t-\t-",
     ));
 
     Ok(())
@@ -93,16 +70,16 @@ fn valid_output_string_default_filters_region_threshold_pass(
 ) -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
     cmd.args(vec![
-        "--paf",
+        "--alignment",
         "tests/cases/test_ok.paf",
         "--fasta",
         "tests/cases/test_paf_ok.fasta",
-        "-r",
+        "--regions",
         "2",
     ]);
 
     cmd.assert().success().stdout(predicate::str::contains(
-        "21172389_LCMV_L-segment_final\t2\t1\t2\t273\t7194\t0.0379\t",
+        "21172389_LCMV_L-segment_final\t3\t2\t3\t321\t7194\t0.0446\t-\t-",
     ));
 
     Ok(())
@@ -113,11 +90,11 @@ fn valid_output_string_default_filters_region_threshold_none(
 ) -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
     cmd.args(vec![
-        "--paf",
+        "--alignment",
         "tests/cases/test_ok.paf",
         "--fasta",
         "tests/cases/test_paf_ok.fasta",
-        "-r",
+        "--regions",
         "3",
     ]);
 
@@ -131,16 +108,16 @@ fn valid_output_string_default_filters_refseq_length_pass() -> Result<(), Box<dy
 {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
     cmd.args(vec![
-        "--paf",
+        "--alignment",
         "tests/cases/test_ok.paf",
         "--fasta",
         "tests/cases/test_paf_ok.fasta",
-        "-s",
+        "--length",
         "5000",
     ]);
 
     cmd.assert().success().stdout(predicate::str::contains(
-        "21172389_LCMV_L-segment_final\t2\t1\t2\t273\t7194\t0.0379\t",
+        "21172389_LCMV_L-segment_final\t3\t2\t3\t321\t7194\t0.0446\t-\t-",
     ));
 
     Ok(())
@@ -151,11 +128,11 @@ fn valid_output_string_default_filters_refseq_length_none() -> Result<(), Box<dy
 {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
     cmd.args(vec![
-        "--paf",
+        "--alignment",
         "tests/cases/test_ok.paf",
         "--fasta",
         "tests/cases/test_paf_ok.fasta",
-        "-s",
+        "--length",
         "8000",
     ]);
 
